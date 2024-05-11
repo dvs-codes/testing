a94c9785b4d5dcef239dcb1adcdc4f63b5ff819f
dvs-codes <132570673+dvs-codes@users.noreply.github.com>
2024-05-11 10:53:12 +0530

    add capitalise and reversestring function and their succesful test.

--- src/app_logic.js
+++ src/app_logic.js
@@ -1,1 +1,18 @@
 
+function capitalize(string) {
+    let firstLetter = string[0].toUpperCase()
+    let result = firstLetter + string.substr(1)
+
+    return result
+}
+
+function reverseString(string) {
+    let final = ""
+    for (let i=1; i<=string.length; i++) {
+        let tempstring = string.charAt(string.length-i)
+        final = final.concat(tempstring)
+    }
+    return final
+}
+
+export {capitalize, reverseString}
---
+++ src/app_logic.test.js
@@ -0,0 +1,9 @@
+import { capitalize, reverseString } from "./app_logic";
+
+// test("capitalize the first character of string ", ()=> {
+//     expect(capitalize("lion")).toBe("Lion");
+// })
+
+test("reverse the string ", ()=> {
+    expect(reverseString("joker")).toBe("rekoj");
+})
